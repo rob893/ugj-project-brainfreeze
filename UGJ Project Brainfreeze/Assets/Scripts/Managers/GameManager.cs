0a2e5795ba2a254ad757eff4dd36dbc75efd0231
Robert Herber <rwherber@gmail.com>
2020-09-04 07:46:14 -0700

    chore: update unity version

---
+++ UGJ Project Brainfreeze/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private GameManager() { }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
